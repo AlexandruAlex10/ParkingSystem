013b136588f2bd51bbe9f393db09978300f39dae
AlexandruAlex10 <alexandru.mitrofan@student.upt.ro>
2024-04-17 17:54:23 +0300

    Implement Forgot-Password Page

--- ParkingSystem/src/app/components/forgot-password/forgot-password.component.ts
+++ ParkingSystem/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../shared/services/auth.service";
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent {
-
+export class ForgotPasswordComponent implements OnInit {
+  constructor(
+    public authService: AuthService
+  ) { }
+  ngOnInit() {
+  }
 }
