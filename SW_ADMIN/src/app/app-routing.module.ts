1cda57ce3824d6b3584f2ae9605781518faa1a86
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2024-06-04 23:46:26 +0300

    Deactivate Sign-Up and Verify-Email Components

--- SW_ADMIN/src/app/app-routing.module.ts
+++ SW_ADMIN/src/app/app-routing.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from "@angular/core";
 import { HomeComponent } from "./components/home/home.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { SignInComponent } from "./components/sign-in/sign-in.component";
-import { SignUpComponent } from "./components/sign-up/sign-up.component";
+// import { SignUpComponent } from "./components/sign-up/sign-up.component";
 import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
-import { VerifyEmailComponent } from "./components/verify-email/verify-email.component";
+// import { VerifyEmailComponent } from "./components/verify-email/verify-email.component";
 import { AuthGuard } from "./shared/guard/auth.guard";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent },
+  // { path: 'sign-up', component: SignUpComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'verify-email', component: VerifyEmailComponent },
+  // { path: 'verify-email', component: VerifyEmailComponent },
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
 ];
 
